8ebab9274edbea5848cd096baea53550c7ef6f8f
ViraOsina <osinka.v@gmail.com>
2021-06-29 22:26:30 +0300

    fixed as per comments

--- HW5.Arrays.Methods/hw5.js
+++ HW5.Arrays.Methods/hw5.js
@@ -3,13 +3,17 @@
 //max – максимальне значення цілого числа. 
 
 function getRandomArray(length, min, max) {
-    const minNumber = Math.min(min, max);
-    const maxNumber = Math.max(min, max);
     let randomArray = [];
+
     for (let i = 0; i < length; i++) {
-        randomArray[i] = [Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber];
+        randomArray = [...randomArray, randomNumber(min, max)];
     }
-    return randomArray.join().split();
+
+    return randomArray;
+}
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 console.log("task 1. get random array", getRandomArray(15, 1, 100));
@@ -21,12 +25,13 @@ function getModa(...numbers) {
     let modes = [];
     let count = [];
     let maxIndex = 0;
-    for (i = 0; i < numbers.length; i++) {
-        let number = numbers[i];
+    const integerNumbers = numbers.filter(el => Number.isInteger(el));
+
+    for (i = 0; i < integerNumbers.length; i++) {
+        let number = integerNumbers[i];
         count[number] = (count[number] || 0) + 1;
-        if (!Number.isInteger(number)) {
-            return 'Inaccurate input! Please use integer digits only';
-        } else if (count[number] > maxIndex) {
+        
+        if (count[number] > maxIndex) {
             maxIndex = count[number];
         }
     }
@@ -37,23 +42,21 @@ function getModa(...numbers) {
         }
     }
 
-    return modes;
+    return +modes;
 }
-console.log("task 2", "mode = ", getModa("w", 6, 55, 11, 78, 6, 55, 77, 57, 87, 6, 2, 56, 3, 2));
+console.log("task 2", "mode = ", getModa("w", 6, 55, 11.5, 78, 6, 55, 77, 57, 87, 6, 2, 56, 3, 2));
 
 //Створіть функцію getAverage(...numbers) – яка рахує середнє арифметичне всіх переданих в неї аргументів. 
 //НЕЦІЛІ ЧИСЛА ІГНОРУЮТЬСЯ
 function getAverage(...numbers) {
     let total = 0;
-    for (let i = 0; i < numbers.length; i += 1) {
-        let number = numbers[i];
-        if (!Number.isInteger(number)) {
-            return 'Inaccurate input! Please use integer digits only';
-        } else {
-            total += numbers[i];
+    const integerNumbers = numbers.filter(el => Number.isInteger(el));
+
+    for (let i = 0; i < integerNumbers.length; i += 1) {
+            total += integerNumbers[i];
         }
-    }
-    return total / numbers.length;
+
+    return total / integerNumbers.length;
 }
 console.log("task 3", "average =", getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 
@@ -61,58 +64,40 @@ console.log("task 3", "average =", getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 8
 //НЕЦІЛІ ЧИСЛА ІГНОРУЮТЬСЯ
 function getMedian(...numbers) {
     let median = 0;
-    const arrayLength = numbers.length;
-    for (let i = 0; i < numbers.length; i++) {
-        let number = numbers[i];
-        if (!Number.isInteger(number)) {
-            return 'Inaccurate input! Please use integer digits only';
-        } else if (arrayLength % 2 === 0) {
-            median = (numbers[arrayLength / 2 - 1] + numbers[arrayLength / 2]) / 2;
+    const integerNumbers = numbers.filter(el => Number.isInteger(el));
+    const arrayLength = integerNumbers.length;
+
+    for (let i = 0; i < integerNumbers.length; i++) {
+        if (arrayLength % 2 === 0) {
+            median = (integerNumbers[arrayLength / 2 - 1] + integerNumbers[arrayLength / 2]) / 2;
             return median;
-        } median = numbers[(arrayLength - 1) / 2];
+        } median = integerNumbers[(arrayLength - 1) / 2];
         return median;
     }
 }
 console.log("task 4", "median =", getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3));
 
 //Створіть функцію filterEvenNumbers(...numbers) – яка фільтрує парні числа передані як аргументи функції
-function filterEvenNumbers(...numbers) {
-    for (let i = 0; i < numbers.length; i++) {
-        let number = numbers[i];
-        if (!Number.isInteger(number)) {
-            return 'Inaccurate input! Please use integer digits only';
-        } else {
-            return numbers.filter(number => number % 2 != 0);
-        }
-    }
+
+function filterEvenNumbers(...numbers){
+    return numbers.filter(number => number % 2 != 0);
 }
+
 console.log("task 5", "filtered even numbers", filterEvenNumbers(1, 2, 3, 4, 5, 6));
 
 //Створіть функцію countPositiveNumbers(...numbers) – яка порахує кількість чисел більших 0
 function countPositiveNumbers(...numbers) {
-    for (let i = 0; i < numbers.length; i++) {
-        let number = numbers[i];
-        if (!Number.isInteger(number)) {
-            return 'Inaccurate input! Please use integer digits only';
-        } else {
-            return numbers.filter(number => number > 0).length;
-        }
-    }
+    return numbers.filter(number => number > 0).length;
 }
+
 console.log("task 6", "count positive numbers", countPositiveNumbers(1, -2, 3, -4, -5, 6));
 
 //Створіть функцію getDividedByFive(...numbers) – яка відфільтрує усі елементи в масиві 
 //та залишить тільки ті, які діляться на ціло на 5
 function getDividedByFive(...numbers) {
-    for (let i = 0; i < numbers.length; i++) {
-        let number = numbers[i];
-        if (!Number.isInteger(number)) {
-            return 'Inaccurate input! Please use integer digits only';
-        } else {
-            return numbers.filter(number => number % 5 == 0);
-        }
-    }
+    return numbers.filter(number => number % 5 == 0);
 }
+
 console.log("task 7", "divided by 5", getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 
 //Створіть функцію replaceBadWords(string) – яка 1) розіб'є фразу на слова, 
@@ -130,11 +115,14 @@ console.log("task 8", "editing bad words", replaceBadWords("It's bullshit!"));
 //Створіть функцію divideByThree(word), яка розбиває кожне слово на умовні склади по 3 букви.
 function divideByThree(word) {
     let output = word.toLowerCase().replaceAll(' ', '');
+
     if (output.length > 3) {
         output = [];
+
         for (let i = 0; i < word.length; i += 3)
             output.push(word.substring(i, i + 3));
     }
+
     return output;
 }
 console.log("task 9", "divide by 3 letters", divideByThree("Commander"));
@@ -148,6 +136,7 @@ function generateCombinations(word) {
     function permutation(word) {
         const letters = word.split('');
         let combinations = [];
+
         if (letters.length === 1) return letters;
 
         for (let i = 0; i < letters.length; i++) {
@@ -157,7 +146,8 @@ function generateCombinations(word) {
 
             for (let j = 0; j < restLettersCombinations.length; j++) {
                 const combinationArray = [firstLetter].concat(restLettersCombinations[j]);
-                let newWord = combinationArray.join('')
+                let newWord = combinationArray.join('');
+
                 if (combinations.includes(newWord)) continue;
                 else combinations.push(newWord);
             }
