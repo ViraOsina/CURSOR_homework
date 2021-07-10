1a309bf1a655ff40cfb988fbe061aa251e051471
ViraOsina <osinka.v@gmail.com>
2021-07-10 22:10:47 +0300

    added play on click function

--- HW10.Events/hw10.js
+++ HW10.Events/hw10.js
@@ -4,6 +4,7 @@ const audio = document.querySelectorAll('audio');
 function startPlaying(e) {
     stopPlaying();
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    
     audio.forEach((item) => {
         if (item.getAttribute("data-key") == e.keyCode) {
           item.play();
@@ -27,4 +28,19 @@ function removeTransition(e) {
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 
 document.addEventListener("keydown", startPlaying);
+
+function playOnClick() {
+    stopPlaying();
+    const keyId = this.getAttribute("data-key");
+    
+    for (let i = 0; i < audio.length; i++) {
+        if (audio[i].getAttribute("data-key") == keyId) {
+            audio[i].play();
+            this.classList.add("playing");
+        } 
+    }
+}
+
+keys.forEach(key => key.addEventListener("click", playOnClick));
+
 
